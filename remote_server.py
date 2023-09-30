9727a131ed0fb19e7f23ca0d3a02e1be04e521e3
Michał Siedlecki <siedlecki.michal@gmail.com>
2023-09-30 18:51:16 +0200

    clean up the remote server file

--- remote_server.py
+++ remote_server.py
@@ -13,30 +13,16 @@ from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 # from sqlalchemy import create_engine
 # from sqlalchemy.orm import sessionmaker, declarative_base
-import aiohttp
 
 from pymongo import MongoClient
 
-templates = Jinja2Templates(directory='templates')
+TEMPLATES = Jinja2Templates(directory='templates')
 APP_SECRET_KEY = '1234'
-
-# Create database or connect to existing one
-# engine = create_engine(DATABASE_URL)
-# session_local = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-# BaseModel = declarative_base()
-#
-# BaseModel.metadata.create_all(bind=engine)
-
-database = {"adam": "1234", "janina": "1234", "sławek": "1234"}
-
-db_connection = MongoClient("mongodb://localhost:27017")
-collection = db_connection.users["users"]
-# collection = db["users"]
+DB_CONNECTION = MongoClient("mongodb://localhost:27017")
+COLLECTION = DB_CONNECTION.users["users"]
 
 
 security = HTTPBasic()
-
-
 app = FastAPI(
     title="HiLo",
     secret_key=APP_SECRET_KEY,
@@ -53,30 +39,32 @@ app = FastAPI(
 class PublicKey(BaseModel):
     key: str
 
-class User(BaseModel):
-    name: str
-    password: str
-
-
-def user_serializer(user) -> dict:
-    return {"name": user["name"], "password": user["password"]}
-
-
-def users_serializer(users) -> list:
-    return [user_serializer(user) for user in users]
+# class User(BaseModel):
+#     name: str
+#     password: str
+#
+#
+# def user_serializer(user) -> dict:
+#     return {"name": user["name"], "password": user["password"]}
+#
+#
+# def users_serializer(users) -> list:
+#     return [user_serializer(user) for user in users]
 
 
 @app.get("/")
 def remote_panel(credentials: Annotated[HTTPBasicCredentials, Depends(security)]):
     """User login for remote panel"""
 
-    result = collection.find_one({"name": credentials.username})
+    result = COLLECTION.find_one({"name": credentials.username})
     if not result:
         raise HTTPException(status_code=403, detail="Forbidden")
+    if result['password'] == credentials.password and not result['has_key']:
+        name = result['name']
+        return TEMPLATES.TemplateResponse('public_key_input.html', {"name": name})
     if result['password'] == credentials.password:
         name = result['name']
-        return {f"Cześć {name}"}
-        # wysyla request o status_ode
+        return TEMPLATES.TemplateResponse('public_key_input.html', {"name": name})
     raise HTTPException(status_code=403, detail="Forbidden")
 
 def is_valid_key(text: str):
@@ -95,16 +83,16 @@ def remote_panel(credentials: Annotated[HTTPBasicCredentials, Depends(security)]
         f.write(key)
     return status.HTTP_201_CREATED
 
-
-async def get_status_code(url: str):
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url) as response:
-            return response.status
-
-@app.get('/show_status', response_class=HTMLResponse)
-async def show_status(request: Request):
-    try:
-        status = await get_status_code('https://martaclose.hi-lo.pl/')
-    except:
-        status = 'Not connected'
-    return templates.TemplateResponse('status_template.html', {"request": request, 'status': status})
+#
+# async def get_status_code(url: str):
+#     async with aiohttp.ClientSession() as session:
+#         async with session.get(url) as response:
+#             return response.status
+#
+# @app.get('/show_status', response_class=HTMLResponse)
+# async def show_status(request: Request):
+#     try:
+#         status = await get_status_code('https://martaclose.hi-lo.pl/')
+#     except:
+#         status = 'Not connected'
+#     return templates.TemplateResponse('status_template.html', {"request": request, 'status': status})
