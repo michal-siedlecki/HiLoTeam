95dd7298b425ab154ed71912b7fd77d7a7fd6249
Michał Siedlecki <siedlecki.michal@gmail.com>
2023-09-30 21:11:49 +0200

    format

--- test_remote_server.py
+++ test_remote_server.py
@@ -4,20 +4,20 @@ import requests
 
 def test_user_can_login():
     name = "janusz"
-    r = requests.get("http://127.0.0.1:8000", auth=(name, "1234"))
+    r = requests.get("http://127.0.0.1:8000/panel", auth=(name, "1234"))
     assert name in r.content.decode("utf-8")
 
 
 def test_user_can_not_login():
     name = "janusz"
-    r = requests.get("http://127.0.0.1:8000", auth=(name, "1234999"))
+    r = requests.get("http://127.0.0.1:8000/panel", auth=(name, "1234999"))
     assert r.status_code == 403
 
 
 def test_user_can_put_key():
     name = "janusz"
-    data = {"key": "123456"}
-    requests.post("http://127.0.0.1:8000", auth=(name, "1234"), json=data)
+    data = {"key_text": "123456"}
+    requests.post("http://127.0.0.1:8000/panel", auth=(name, "1234"), data=data)
     filename = f"test_{name}_id_rsa.pub"
     assert filename in os.listdir(".")
     os.remove(filename)
