c6477342ad1165c2159019fa36d98ba1a35ec00a
Michał Siedlecki <siedlecki.michal@gmail.com>
2023-09-30 11:29:59 +0200

    add mongo db to remote server

--- remote_server.py
+++ remote_server.py
@@ -1,30 +1,36 @@
 """Start serving the application by command
 `uvicorn remote_server:app --reload`
+for local testing first run mongodb locally
+docker run -d -p 27017:27017 --name hilo-mongo mongo:latest
 """
-import os
 from typing import Annotated
-
+from pydantic import BaseModel
 from fastapi import Depends, FastAPI
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
 from starlette import status
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+# from sqlalchemy import create_engine
+# from sqlalchemy.orm import sessionmaker, declarative_base
 
-# Database which persists users and passwords for remote login
-DATABASE_URL = os.getenv('DATABASE_URL')
-APP_SECRET_KEY = os.getenv('APP_SECRET_KEY')
+from pymongo import MongoClient
+
+
+APP_SECRET_KEY = '1234'
 
 # Create database or connect to existing one
-engine = create_engine(DATABASE_URL)
-session_local = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-BaseModel = declarative_base()
+# engine = create_engine(DATABASE_URL)
+# session_local = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+# BaseModel = declarative_base()
+#
+# BaseModel.metadata.create_all(bind=engine)
 
-BaseModel.metadata.create_all(bind=engine)
+database = {"adam": "1234", "janina": "1234", "sławek": "1234"}
+
+db_connection = MongoClient("mongodb://localhost:27017")
+db = db_connection.database_name
+collection = db["users"]
 
 
 security = HTTPBasic()
-
-database = {"adam": "1234", "janina": "1234", "sławek": "1234"}
 
 
 app = FastAPI(
@@ -41,15 +47,32 @@ app = FastAPI(
 )
 
 
+class User(BaseModel):
+    name: str
+    password: str
+
+
+def user_serializer(user) -> dict:
+    return {"id": str(user["_id"]), "name": user["name"], "password": user["password"]}
+
+
+def users_serializer(users) -> list:
+    return [user_serializer(user) for user in users]
+
+
 @app.get("/", status_code=status.HTTP_200_OK)
 def remote_panel(credentials: Annotated[HTTPBasicCredentials, Depends(security)]):
     """User login for remote panel"""
-    user = credentials.username
-    if user not in database.keys():
-        return status.HTTP_403_FORBIDDEN
-    if database.get(user) == credentials.password:
-        return {f"Welcome {user} to remote panel"}
-    return status.HTTP_403_FORBIDDEN
+    name = credentials.username
+    user = users_serializer(collection.find({"name": name}))
+    return user
+    # if user not in database.keys():
+    # if not user not in database.keys():
+    #     return status.HTTP_403_FORBIDDEN
+    # if database.get(user) == credentials.password:
+    #     return {f"Welcome {user} to remote panel"}
+    # return status.HTTP_403_FORBIDDEN
+
 
 @app.get("/key", status_code=status.HTTP_200_OK)
 def remote_panel(credentials: Annotated[HTTPBasicCredentials, Depends(security)]):
