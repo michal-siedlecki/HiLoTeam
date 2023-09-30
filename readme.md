47575ce76883960193d5a712f92024b66f79932d
Michał Siedlecki <siedlecki.michal@gmail.com>
2023-09-30 10:21:00 +0200

    remove docker and add workflow to readme

--- readme.md
+++ readme.md
@@ -2,3 +2,15 @@ HiLo Team Readme file
 
 Roadmap
 * tbd
+
+
+User Pobiera kod na kartę sd
+User Generuje sobie klucze na raspbiaku i wyświetla mu się klucz publiczny
+User prosi o rejestrację
+My tworzymy w bazie credentiale dla użytkownika 
+Wysyłamy je
+On wchodzi na panel konfiguracyjny 
+Wkleja klucz publiczny i klika submit
+Gdy kliknie submit uruchamia skrypt który kopiuje ten klucz do wlasciwego katalogu
+Następnie użytkownik ze swojego raspiaka nawiązuje połączenie ssh i patrzy w panelu zdalnym czy się udało
+Jeśli się udało powinien zobaczyć swojego nighscouta wchodząc na swój panel 'publiczny'
--- remote_server.py
+++ remote_server.py
@@ -1,7 +1,7 @@
 """Start serving the application by command
 `uvicorn remote_server:app --reload`
 """
-
+import os
 from typing import Annotated
 
 from fastapi import Depends, FastAPI
@@ -11,9 +11,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 # Database which persists users and passwords for remote login
-DATABASE_URL = ""
-APP_SECRET_KEY = "abc123"
+DATABASE_URL = os.getenv('DATABASE_URL')
+APP_SECRET_KEY = os.getenv('APP_SECRET_KEY')
 
+# Create database or connect to existing one
 engine = create_engine(DATABASE_URL)
 session_local = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 BaseModel = declarative_base()
@@ -47,5 +48,15 @@ def remote_panel(credentials: Annotated[HTTPBasicCredentials, Depends(security)]
     if user not in database.keys():
         return status.HTTP_403_FORBIDDEN
     if database.get(user) == credentials.password:
-        return {"Welcome to remote panel"}
+        return {f"Welcome {user} to remote panel"}
+    return status.HTTP_403_FORBIDDEN
+
+@app.get("/key", status_code=status.HTTP_200_OK)
+def remote_panel(credentials: Annotated[HTTPBasicCredentials, Depends(security)]):
+    """User login for remote panel"""
+    user = credentials.username
+    if user not in database.keys():
+        return status.HTTP_403_FORBIDDEN
+    if database.get(user) == credentials.password:
+        return {f"Welcome {user} to remote panel"}
     return status.HTTP_403_FORBIDDEN
